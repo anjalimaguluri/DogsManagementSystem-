f021c124227c50bd81bac2d88f124e8efdc09ab8
anjalimaguluri <magulurinaganjali@gmail.com>
2023-11-12 16:43:40 -0600

    first commit

---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+
+/**
+ * @author Naganjali Maguluri
+ * Nov 11, 2023
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
